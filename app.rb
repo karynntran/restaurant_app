4915e1ebe2515abab3af1d3e8da80d7044c0ec9c
karynntran <karynn.tran@gmail.com>
2014-10-14 14:25:18 -0400

    Added links for navigation between food pages
    Allows users to see all food items or specific foods through links on page

--- app.rb
+++ app.rb
@@ -1,12 +1,99 @@
 require 'bundler'
 Bundler.require
-require 'activesupport'
+require 'active_support'
 
 require_relative 'models/food.rb'
 require_relative 'models/party.rb'
 
 ActiveRecord::Base.establish_connection({
 	adapter: 'postgresql',
-	database: ''
+	database: 'food_db'
 })
 
+### LANDING PAGE ###
+
+get '/' do
+
+end
+
+### FOOD CRUD ###
+
+get '/foods' do
+	@foods = Food.all
+	erb :'food/index'
+end
+
+get '/foods/new' do
+	erb :'food/new'
+end
+
+post '/foods' do
+	@food = Food.create(params[:food])
+	redirect '/foods'
+end
+
+get '/foods/:id/edit' do
+	@food = Food.find(params[:id])
+	erb :'food/edit'
+end
+
+patch '/foods/:id' do
+	food = Food.find(params[:id])
+	food.update(params[:food])
+	redirect '/foods/#{food.id}'
+end
+
+get '/foods/:id' do
+	@food = Food.find(params[:id])
+	erb :'food/show'
+
+end
+
+delete '/foods/:id' do
+	food = Food.destroy(params[:id])
+	redirect '/foods'
+end
+
+### PARTY CRUD ###
+
+# get '/parties' do
+# end
+
+# get '/parties/new' do
+# end
+
+# post '/parties' do
+# end
+
+# get '/parties/:id/edit' do
+# end
+
+# patch '/parties/:id' do
+# end
+
+# get '/parties/:id' do
+# end
+
+# delete '/parties/:id' do
+# end
+
+# ### OTHER ###
+# post '/orders' do
+# 	#Creates a new order
+# end
+
+# patch '/orders/:id' do
+# 	#Change item to no-charge
+# end
+
+# delete '/orders' do
+# 	#Removes an order
+# end
+
+# get '/parties/:id/receipt' do
+# 	#Saves the party's receipt data to a file. Displays the content of the receipt. Offer the file for download.
+# end
+
+# patch '/parties/:id/checkout' do
+# 	#marks the party as paid
+# end
